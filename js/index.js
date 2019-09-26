b132e9d2035714fe0a67e67e44417a03113b86c5
dennyerikson <dennyeriks00on@gmail.com>
2019-09-25 21:48:41 -0300

    Aula concluida

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 // create var tbody
-
+var tbody = document.getElementById('tbody');
 //getJson
-
-
+$.getJSON("./pets.json", function(json){
+    json.map(getPet).forEach(function(item){
+        item = item.split("-");
+        tbody.innerHTML +=
+        "<tr>"
+            +"<td> <img src='./img/"+item[0]+"' alt='img'></td>"
+            +"<td>"+item[1]+"</td>"
+            +"<td>"+item[2]+"</td>"
+            +"<td>"+item[3]+"</td>"
+        +"</tr>"
+    });
+});
 //serializer
+function getPet(item){
+    return [
+        item.img, item.name, item.breed, item.sex
+    ].join("-")
+}
